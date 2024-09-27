acf1bc67321010dc207aa548b46a6579afa600b2
wulfric <90691032+ferdykp@users.noreply.github.com>
2024-09-27 22:27:04 +0700

    Update booking.blade.php

--- resources/views/auth/booking.blade.php
+++ resources/views/auth/booking.blade.php
@@ -247,6 +247,18 @@
                     </div>
     
                     <div class="mt-8">
+
+                        <div class="w-full ">
+                            <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                                <div class="sm:col-span-4">
+                                    <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                                        <div class="mt-2">
+                                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-3 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
     
                     </div>
     
