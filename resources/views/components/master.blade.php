688935589538e9190f6a37ca48edc6cb77e2fad5
Miqdad <ahmadkimpir@gmail.com>
2024-10-12 01:09:22 +0700

    create master component

---
+++ resources/views/components/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $pagetitle }}</title>
+    @vite('resources/css/app.css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <x-sidebar>
+        </x-sidebar>
+        <x-navbar>
+        </x-navbar>
+        <x-main>
+        </x-main>
+    </div>
+    </div>
+    </div>
+</body>
+
+</html>
